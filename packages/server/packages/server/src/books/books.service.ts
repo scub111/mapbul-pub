0be1d6616e7e2a9e38c690796b16b34af4cb96f6
scub111 <scub111@mail.ru>
2019-08-25 23:16:30 +0300

    Some improvements.

--- packages/server/packages/server/src/books/books.service.ts
+++ packages/server/packages/server/src/books/books.service.ts
@@ -3,41 +3,48 @@ import { BOOKS } from '../mocks/books.mock';
 
 @Injectable()
 export class BooksService {
-    books = BOOKS;
+  books = BOOKS;
 
-    getBooks(): Promise<any> {
-       return new Promise(resolve => {
-          resolve(this.books);
-       });
-    }
+  getBooks(): Promise<any> {
+    return new Promise(resolve => {
+      resolve(this.books);
+    });
+  }
 
-    getBook(bookId): Promise<any> {
-       const id = Number(bookId);
-       return new Promise(resolve => {
-          const book = this.books.find(item => item.id === id);
-          if (!book) {
-             throw new HttpException('Book does not exist', 404);
-          }
-          resolve(book);
-       });
-    }
+  deleteBooks(): Promise<any> {
+    return new Promise(resolve => {
+      this.books = [];
+      resolve(this.books);
+    });
+  }
 
-    addBooks(book): Promise<any> {
-       return new Promise(resolve => {
-          this.books.push(book);
-          resolve(this.books);
-       });
-    }
+  getBook(bookId): Promise<any> {
+    const id = Number(bookId);
+    return new Promise(resolve => {
+      const book = this.books.find(item => item.id === id);
+      if (!book) {
+        throw new HttpException('Book does not exist', 404);
+      }
+      resolve(book);
+    });
+  }
 
-    deleteBook(bookId): Promise<any> {
-       const id = Number(bookId);
-       return new Promise(resolve => {
-          const index = this.books.findIndex(item => item.id === id);
-          if (index === -1) {
-             throw new HttpException('Books does not exist', 404);
-          }
-          this.books.slice(index, 1);
-          resolve(this.books);
-       });
-    }
+  addBooks(book): Promise<any> {
+    return new Promise(resolve => {
+      this.books.push(book);
+      resolve();
+    });
+  }
+
+  deleteBook(bookId): Promise<any> {
+    const id = Number(bookId);
+    return new Promise(resolve => {
+      const index = this.books.findIndex(item => item.id === id);
+      if (index === -1) {
+        throw new HttpException('Books does not exist', 404);
+      }
+      this.books.splice(index, 1);
+      resolve();
+    });
+  }
 }
