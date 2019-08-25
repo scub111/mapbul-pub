fa0d840e2096d495ef46aa0540985b3f0f23bde2
scub111 <scub111@mail.ru>
2019-08-25 23:16:30 +0300

    Some improvements.

--- packages/common/packages/server/src/books/books.controller.ts
+++ packages/common/packages/server/src/books/books.controller.ts
@@ -1,33 +1,46 @@
-import { Controller, Get, Param, Post, Body, Delete, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Body,
+  Delete,
+  Query,
+} from '@nestjs/common';
 import { BooksService } from './books.service';
 import { CreateBookDTO } from './dto/create-book.dto';
 
 @Controller('books')
 export class BooksController {
-   constructor(private booksService: BooksService) {}
+  constructor(private booksService: BooksService) {}
 
-   @Get()
-   async getBooks() {
-      const books = await this.booksService.getBooks();
-      return books;
-   }
+  @Get()
+  async getBooks() {
+    const books = await this.booksService.getBooks();
+    return books;
+  }
 
-   @Get(':bookId')
-   async getBook(@Param('bookId') bookId) {
-      const book = await this.booksService.getBook(bookId);
-      return book;
-   }
+  @Post()
+  async addBook(@Body() createBookDTO: CreateBookDTO) {
+    const newBook = { ...createBookDTO, ...{ author: 'test 123+++' } };
+    const book = await this.booksService.addBooks(newBook);
 
-   @Post()
-   async addBooks(@Body() createBookDTO: CreateBookDTO) {
-      const newBook = {...createBookDTO, ...{author: 'test 123++'}};
-      const book = await this.booksService.addBooks(newBook);
+    return book;
+  }
 
-      return book;
-   }
+  @Delete()
+  async deleteBooks() {
+    const books = await this.booksService.deleteBooks();
+  }
 
-   @Delete()
-   async deleteBooks(@Query() query) {
-      const books = await this.booksService.deleteBook(query.book)
-   }
+  @Get(':bookId')
+  async getBook(@Param('bookId') bookId) {
+    const book = await this.booksService.getBook(bookId);
+    return book;
+  }
+
+  @Delete(':bookId')
+  async deleteBook(@Param('bookId') bookId) {
+    const books = await this.booksService.deleteBook(bookId);
+  }
 }
