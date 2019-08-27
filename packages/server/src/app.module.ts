dffe11c643b68228132f184d9c9758621e8bf44f
kbiskub <kbiskub@wv00403.local>
2019-08-27 23:23:54 +0300

    Temp commit.

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksService } from './books/books.service';
 import { BooksController } from './books/books.controller';
+import { ArticlesController } from './articles/articles.controller';
+import { ArticlesService } from './articles/articles.service';
 
 @Module({
   imports: [],
-  providers: [AppService, BooksService],
-  controllers: [AppController, BooksController],
+  providers: [AppService, BooksService, ArticlesService],
+  controllers: [AppController, BooksController, ArticlesController],
 })
 export class AppModule {}
