b0716a2630c626180010cb299b3fa71cfefa8361
kbiskub <kbiskub@wv00403.local>
2019-08-28 18:01:28 +0300

    The inversion principle has been integrated into the service.

--- packages/server/packages/server/packages/server/src/articles/articles.controller.ts
+++ packages/server/packages/server/packages/server/src/articles/articles.controller.ts
@@ -1,43 +1,54 @@
-import { Controller, Get, Post, Put, Delete, Param } from '@nestjs/common';
-import { ArticlesService } from './articles.service';
-import { IController } from 'src/common/IController';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Inject,
+} from '@nestjs/common';
+// import { IController } from 'src/common/IController';
 import { IArticleDTO } from './dto/article.dto';
 import { TID } from 'src/common/types';
+import { IService } from 'src/common/IService';
+import { IController } from '../common/IController';
 
 @Controller('articles')
 export class ArticlesController implements IController<IArticleDTO> {
-    constructor(private articleService: ArticlesService) {}
+  constructor(
+    @Inject('IService') private readonly articleService: IService<IArticleDTO>,
+  ) {}
 
-    @Get()
-    async getAll(): Promise<IArticleDTO[]> {
-        return this.articleService.getAll();
-    }
+  @Get()
+  async getAll(): Promise<IArticleDTO[]> {
+    return this.articleService.getAll();
+  }
 
-    @Post()
-    postItem(item: IArticleDTO): IArticleDTO {
-        throw new Error('Method not implemented.');
-    }
+  @Post()
+  postItem(item: IArticleDTO): IArticleDTO {
+    throw new Error('Method not implemented.');
+  }
 
-    @Put()
-    putAll(item: IArticleDTO): IArticleDTO {
-        throw new Error('Method not implemented.');
-    }
+  @Put()
+  putAll(item: IArticleDTO): IArticleDTO {
+    throw new Error('Method not implemented.');
+  }
 
-    getItem(id: TID): IArticleDTO {
-        throw new Error('Method not implemented.');
-    }
+  getItem(id: TID): IArticleDTO {
+    throw new Error('Method not implemented.');
+  }
 
-    @Delete()
-    deleteAll(): void {
-        throw new Error('Method not implemented.');
-    }
+  @Delete()
+  deleteAll(): void {
+    throw new Error('Method not implemented.');
+  }
 
-    @Put(':id')
-    putItem(id: TID, item: IArticleDTO): IArticleDTO {
-        throw new Error('Method not implemented.');
-    }
+  @Put(':id')
+  putItem(id: TID, item: IArticleDTO): IArticleDTO {
+    throw new Error('Method not implemented.');
+  }
 
-    deleteItem(id: TID): IArticleDTO {
-        throw new Error('Method not implemented.');
-    }
+  deleteItem(id: TID): IArticleDTO {
+    throw new Error('Method not implemented.');
+  }
 }
