de62f250535b68b225a4601de4e19ee1c1937ece
kbiskub <kbiskub@wv00403.local>
2019-08-28 18:01:28 +0300

    The inversion principle has been integrated into the service.

--- packages/common/src/articles/articles.service.ts
+++ packages/common/src/articles/articles.service.ts
@@ -5,43 +5,41 @@ import { IService } from 'src/common/IService';
 import { IArticleDTO } from './dto/article.dto';
 import { Connection } from 'mysql';
 import { TID } from 'src/common/types';
+import { dbConnection } from 'src/common/utils';
 
 @Injectable()
 export class ArticlesService implements IService<IArticleDTO> {
-    constructor() {
-        this.connection = mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: '461301+MY',
-            database: 'mapbul',
-        });
-        this.query = util.promisify(this.connection.query).bind(this.connection);
-    }
+  constructor() {
+    this.connection = mysql.createConnection(dbConnection);
+    this.query = util.promisify(this.connection.query).bind(this.connection);
+  }
 
-    connection: Connection;
-    query: any;
+  connection: Connection;
+  query: (expression: string) => Promise<any>;
 
-    async getAll(): Promise<IArticleDTO[]> {
-        const result = await this.query('SELECT id, title from article LIMIT 100');
-        return result.map((i: IArticleDTO) => ({id: i.id, title: i.title} as IArticleDTO));
-    }
+  async getAll(): Promise<IArticleDTO[]> {
+    const result = await this.query('SELECT id, title from article LIMIT 100');
+    return result.map(
+      (i: IArticleDTO) => ({ id: i.id, title: i.title } as IArticleDTO),
+    );
+  }
 
-    postItem(item: IArticleDTO): IArticleDTO {
-        throw new Error('Method not implemented.');
-    }
-    putAll(item: IArticleDTO): IArticleDTO {
-        throw new Error('Method not implemented.');
-    }
-    deleteAll(): void {
-        throw new Error('Method not implemented.');
-    }
-    getItem(id: TID): IArticleDTO {
-        throw new Error('Method not implemented.');
-    }
-    putItem(id: TID): IArticleDTO {
-        throw new Error('Method not implemented.');
-    }
-    deleteItem(id: TID): IArticleDTO {
-        throw new Error('Method not implemented.');
-    }
+  postItem(item: IArticleDTO): IArticleDTO {
+    throw new Error('Method not implemented.');
+  }
+  putAll(item: IArticleDTO): IArticleDTO {
+    throw new Error('Method not implemented.');
+  }
+  deleteAll(): void {
+    throw new Error('Method not implemented.');
+  }
+  getItem(id: TID): IArticleDTO {
+    throw new Error('Method not implemented.');
+  }
+  putItem(id: TID): IArticleDTO {
+    throw new Error('Method not implemented.');
+  }
+  deleteItem(id: TID): IArticleDTO {
+    throw new Error('Method not implemented.');
+  }
 }
--- packages/common/src/common/IController.ts
+++ packages/common/src/common/IController.ts
@@ -1,11 +1,11 @@
 import { TID } from './types';
 
 export interface IController<T> {
-    getAll(): Promise<T[]> | T[];
-    postItem(item: T): T;
-    putAll(item: T): T;
-    deleteAll(): void;
-    getItem(id: TID): T;
-    putItem(id: TID, item: T): T;
-    deleteItem(id: TID): T;
+  getAll(): Promise<T[]> | T[];
+  postItem(item: T): T;
+  putAll(item: T): T;
+  deleteAll(): void;
+  getItem(id: TID): T;
+  putItem(id: TID, item: T): T;
+  deleteItem(id: TID): T;
 }
