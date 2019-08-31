09878579c52e28819aa3d8cbac9d5e029f7fa39d
scub111 <scub111@mail.ru>
2019-08-31 20:31:30 +0300

    Package.json improvements.

--- packages/common/src/main.ts
+++ packages/common/src/main.ts
@@ -3,7 +3,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = 3100;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3100);
+  await app.listen(port);
+  console.log(`server started at http://localhost:${port}`);
 }
 bootstrap();
