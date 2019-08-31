60b8e7da8057703290cd98e54f83a22c8301f3f5
scub111 <scub111@mail.ru>
2019-08-31 17:30:44 +0300

    Temp commit

--- packages/server/packages/server/_templates/server/dto/index.ejs.t
+++
@@ -1,7 +0,0 @@
----
-to: src/app/<%= name %>.dto.ts
----
-export interface I<%= name %>DTO {
-  readonly <%= message %>: number;
-  readonly <%= abc %>: string;
-}
--- packages/server/packages/server/src/mocks/books.mock.ts
+++ packages/server/packages/server/src/mocks/books.mock.ts
@@ -3,36 +3,36 @@ export const BOOKS = [
     id: 1,
     title: 'First book',
     description: 'This is the description for the first book',
-    author: 'Olususi Oluyemi'
+    author: 'Olususi Oluyemi',
   },
   {
     id: 2,
     title: 'Second book',
     description: 'This is the description for the second book',
-    author: 'John Barry'
+    author: 'John Barry',
   },
   {
     id: 3,
     title: 'Third book',
     description: 'This is the description for the third book',
-    author: 'Clement Wilfred'
+    author: 'Clement Wilfred',
   },
   {
     id: 4,
     title: 'Fourth book',
     description: 'This is the description for the fourth book',
-    author: 'Christian nwamba'
+    author: 'Christian nwamba',
   },
   {
     id: 5,
     title: 'Fifth book',
     description: 'This is the description for the fifth book',
-    author: 'Chris anderson'
+    author: 'Chris anderson',
   },
   {
     id: 6,
     title: 'Sixth book',
     description: 'This is the description for the sixth book',
-    author: 'Olususi Oluyemi'
-  }
+    author: 'Olususi Oluyemi',
+  },
 ];
