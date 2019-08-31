2e00ed78dab51ee8eafbf9efc93390ae59a2a3bd
scub111 <scub111@mail.ru>
2019-08-31 20:31:30 +0300

    Package.json improvements.

--- packages/server/src/express.ts
+++ packages/server/src/express.ts
@@ -1,14 +1,14 @@
-// import express from 'express';
+import express from 'express';
 
-// const app = express();
-// const port = 3000; // default port to listen
+const app = express();
+const port = 3100; // default port to listen
 
-// // define a route handler for the default home page
-// app.get('/', (req, res) => {
-//   res.send('Hello world!');
-// });
+// define a route handler for the default home page
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+});
 
-// // start the Express server
-// app.listen(port, () => {
-//   console.log(`server started at http://localhost:${port}`);
-// });
+// start the Express server
+app.listen(port, () => {
+  console.log(`server started at http://localhost:${port}`);
+});
