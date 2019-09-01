c80fc9414e36ff4ddfda0a6b64411b087c579b76
scub111 <scub111@mail.ru>
2019-09-02 01:01:42 +0300

    Config was added.

--- packages/server/packages/common/packages/server/src/server/common/utils.ts
+++ packages/server/packages/common/packages/server/src/server/common/utils.ts
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import appRootPath from 'app-root-path';
 
-export const dbConnection = JSON.parse(
-  fs.readFileSync(`${appRootPath.path}/db-connection.json`, 'utf8'),
-);
+// export const dbConnection = JSON.parse(
+//   fs.readFileSync(`${appRootPath.path}/db-connection.json`, 'utf8'),
+// );
