474a34fd9297d90521270f40bc68da73b72262b4
scub111 <scub111@mail.ru>
2019-09-01 08:53:08 +0300

    Temp commit.

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const port = 3100;
+  const port = process.env.PORT || 3100;
   const app = await NestFactory.create(AppModule);
   await app.listen(port);
   console.log(`server started at http://localhost:${port}`);
