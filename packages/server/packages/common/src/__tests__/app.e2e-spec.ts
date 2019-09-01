020dd3c0a36e9eb0b5a1b2db1bad343c1f7a44e4
scub111 <scub111@mail.ru>
2019-09-01 16:29:45 +0300

    Temp commit.

--- packages/server/packages/common/src/__tests__/app.e2e-spec.ts
+++ packages/server/packages/common/src/__tests__/app.e2e-spec.ts
@@ -4,6 +4,7 @@ import { AppModule } from 'src/app.module';
 import { INestApplication } from '@nestjs/common';
 import { ArticlesService } from 'src/articles/articles.service';
 import promisePool from 'es6-promise-pool';
+import axios from 'axios';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -34,25 +35,59 @@ describe('AppController (e2e)', () => {
     return new Promise(resolve => setTimeout(resolve, ms));
   };
 
+  const simulate = async (iter: number, ms: number) => {
+    try {
+      if (iter % 1000 === 0) {
+        throw new Error('Trouble');
+      }
+      await sleep(ms);
+    } catch (e) {
+      // console.log(e);
+    }
+  };
+
+  const get = async (url: string) => {
+    try {
+      // await request(url).get('/');
+      await axios({url});
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   const generatePromises = (iterationCount: number, url: string) =>
     function*() {
       for (let i = 0; i < iterationCount; i++) {
-        yield request(url).get('/');
+        // yield request(url).get('/').catch(e => console.log(e));
+        yield get(url);
+        // yield simulate(i, 1);
         // yield sleep(100);
       }
     };
 
   it.only('/ (GET) 1000', async () => {
-    const iterationCount = 100;
+    const iterationCount = 5000;
     // const url = `http://localhost:3100/articles`;
     const url = `http://pub.mapbul.scub111.com/articles`;
+    // const url = `http://mapbul.scub111.com`;
+    // const url = `http://192.168.0.22:8081/articles`;
+    // const url = `http://192.168.0.22:8082`;
     // const url = `http://localhost:8081/api/values`;
     // const url = `https://www.google.com/`;
-    const threadCount = 10;
+    const threadCount = 50;
     const pool = new promisePool(
       generatePromises(iterationCount, url) as any,
       threadCount,
     );
+
+    pool.addEventListener('fulfilled', (event: any) => {
+      // console.log('Fulfilled: ' + event.data.result);
+    });
+
+    pool.addEventListener('rejected', (event: any) => {
+      console.log('Rejected: ' + event.data.error.message);
+    });
+
     const t0 = new Date();
     await pool.start();
     const diff = new Date().valueOf() - t0.valueOf();
