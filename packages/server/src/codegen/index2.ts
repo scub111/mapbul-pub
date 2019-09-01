c7f1c9f95ca74c21045aa4e13e6416692692bdc6
scub111 <scub111@mail.ru>
2019-09-01 20:29:46 +0300

    Temp commit.

---
+++ packages/server/src/codegen/index2.ts
@@ -0,0 +1,46 @@
+import { Project, StructureKind } from 'ts-morph';
+import appRootPath from 'app-root-path';
+
+const project = new Project({
+  tsConfigFilePath: `${appRootPath.path}/tsconfig.json`,
+});
+
+// add source files
+project.addExistingSourceFiles('src/**/*.ts');
+const myClassFile = project.createSourceFile('src/MyClass.ts', 'export class MyClass {}');
+const myEnumFile = project.createSourceFile('src/MyEnum.ts', {
+    statements: [{
+        kind: StructureKind.Enum,
+        name: 'MyEnum',
+        isExported: true,
+        members: [{ name: 'member' }],
+    }],
+});
+
+// get information
+const myClass = myClassFile.getClassOrThrow('MyClass');
+myClass.getName();          // returns: "MyClass"
+myClass.hasExportKeyword(); // returns: true
+myClass.isDefaultExport();  // returns: false
+
+// manipulate
+const myInterface = myClassFile.addInterface({
+    name: 'IMyInterface',
+    isExported: true,
+    properties: [{
+        name: 'myProp',
+        type: 'number',
+    }],
+});
+
+myClass.rename('NewName');
+myClass.addImplements(myInterface.getName());
+myClass.addProperty({
+    name: 'myProp',
+    initializer: '5',
+});
+
+// project.getSourceFileOrThrow('src/ExistingFile.ts').delete();
+
+// asynchronously save all the changes above
+project.save();
