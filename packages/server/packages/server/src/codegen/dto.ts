703c356febda693aebcdcd67b58cf8e76473ef97
scub111 <scub111@mail.ru>
2019-09-03 23:17:06 +0300

    Generate .dto.ts from sql table.

--- packages/server/packages/server/src/codegen/dto.ts
+++ packages/server/packages/server/src/codegen/dto.ts
@@ -4,7 +4,7 @@ import { IField } from './IField';
 export const createDTO = (project: Project, folder: string, name: string, fields: IField[]) => {
   const fullName = `I${name}DTO`;
   const fileName = `src/server/${folder}/${name.toLowerCase()}.dto.ts`;
-  const properties: Array<OptionalKind<PropertySignatureStructure>> = fields.map(i => ({name: i.name, type: i.type}));
+  const properties: Array<OptionalKind<PropertySignatureStructure>> = fields.map(i => ({name: i.field, type: i.type}));
   const myInterfaceFile = project.createSourceFile(fileName, {
     statements: [{
         kind: StructureKind.Interface,
