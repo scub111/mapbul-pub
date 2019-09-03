2231c88f40661489889e4038fcf3b038c1f2c866
scub111 <scub111@mail.ru>
2019-09-03 23:17:06 +0300

    Generate .dto.ts from sql table.

--- packages/server/packages/server/packages/common/packages/server/src/codegen/index.ts
+++ packages/server/packages/server/packages/common/packages/server/src/codegen/index.ts
@@ -1,19 +1,31 @@
+if (process.env.NODE_ENV !== 'development') {
+  require('module-alias/register');
+  console.log('module-alias/register');
+}
+console.log('test');
+import { setEnvVariables, serverConfig } from 'common/serverConfig';
+setEnvVariables(__dirname + '/.env');
+console.log(serverConfig.dbConnection);
+
 import { Project } from 'ts-morph';
 import { createDTO } from './dto';
 import { IField } from './IField';
+import { getFields } from './getFields';
 import appRootPath from 'app-root-path';
 
-const project = new Project({
-  tsConfigFilePath: `${appRootPath.path}/tsconfig.json`,
-});
+const main = async () => {
+  const project = new Project({
+    tsConfigFilePath: `${appRootPath.path}/tsconfig.json`,
+  });
 
-project.addExistingSourceFiles('src/**/*.ts');
+  const fields = await getFields('article');
 
-const fields: IField[] = [
-  { name: 'id', type: 'string'},
-  { name: 'temp', type: 'string'},
-];
+  project.addExistingSourceFiles('src/**/*.ts');
 
-createDTO(project, 'articles2', 'Article', fields);
+  createDTO(project, 'articles2', 'Article', fields);
 
-project.save();
+  project.save();
+};
+
+main();
+
