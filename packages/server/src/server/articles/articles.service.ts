c5ed3ec11496122982b48aa5b147b82c81fcc5f7
scub111 <scub111@mail.ru>
2019-09-04 01:15:53 +0300

    Template engine was integrated.

--- packages/server/src/server/articles/articles.service.ts
+++ packages/server/src/server/articles/articles.service.ts
@@ -6,7 +6,6 @@ import { IArticleDTO } from 'server/articles/article.dto';
 import { Connection } from 'mysql';
 import { TID } from 'server/common/types';
 import { serverConfig } from 'common/serverConfig';
-// import { dbConnection } from 'server/common/utils';
 
 @Injectable()
 export class ArticlesService implements IService<IArticleDTO> {
@@ -19,7 +18,7 @@ export class ArticlesService implements IService<IArticleDTO> {
   query: (expression: string) => Promise<any>;
 
   async getAll(): Promise<IArticleDTO[]> {
-    const result = await this.query('SELECT id, title from article LIMIT 10');
+    const result = await this.query('SELECT id, title from article LIMIT 100');
     return result.map(
       (i: IArticleDTO) => ({ id: i.id, title: i.title } as IArticleDTO),
     );
