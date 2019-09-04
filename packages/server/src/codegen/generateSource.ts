6cff7d0da93d9b39346bcc2a62853cf255481770
scub111 <scub111@mail.ru>
2019-09-04 23:50:58 +0300

    Temp commit.

---
+++ packages/server/src/codegen/generateSource.ts
@@ -0,0 +1,17 @@
+import handlebars from 'handlebars';
+import fs from 'fs';
+import fse from 'fs-extra';
+
+interface ICreateSorceConfig {
+  templatePath: string;
+  data: any;
+  sourcePath: string;
+}
+
+export const createSorce = (config: ICreateSorceConfig) => {
+  const templateFile = fs.readFileSync(config.templatePath, 'utf-8');
+  const template = handlebars.compile(templateFile);
+  const source = template(config.data);
+  fs.writeFileSync(config.sourcePath, source, {encoding: 'utf8', flag: 'w'});
+  console.log(source);
+};
