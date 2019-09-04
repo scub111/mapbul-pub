18393bd7a9bba86756c2fa7987539efcb6dafb07
scub111 <scub111@mail.ru>
2019-09-04 09:09:52 +0300

    Work on teamplates and its engine.

---
+++ packages/server/packages/server/src/server/articles2/articles.service.ts
@@ -0,0 +1,45 @@
+import * as mysql from 'mysql';
+import * as util from 'util';
+import { Injectable } from '@nestjs/common';
+import { IService } from 'server/common/IService';
+import { IArticleDTO } from 'server/articles/article.dto';
+import { Connection } from 'mysql';
+import { TID } from 'server/common/types';
+import { serverConfig } from 'common/serverConfig';
+
+@Injectable()
+export class ArticlesService implements IService<IArticleDTO> {
+  constructor() {
+    this.connection = mysql.createConnection(serverConfig.dbConnection);
+    this.query = util.promisify(this.connection.query).bind(this.connection);
+  }
+
+  connection: Connection;
+  query: (expression: string) => Promise<any>;
+
+  async getAll(): Promise<IArticleDTO[]> {
+    const result = await this.query('SELECT id, title from article LIMIT 100');
+    return result.map(
+      (i: IArticleDTO) => ({ id: i.id, title: i.title } as IArticleDTO),
+    );
+  }
+
+  postItem(item: IArticleDTO): IArticleDTO {
+    throw new Error('Method not implemented.');
+  }
+  putAll(item: IArticleDTO): IArticleDTO {
+    throw new Error('Method not implemented.');
+  }
+  deleteAll(): void {
+    throw new Error('Method not implemented.');
+  }
+  getItem(id: TID): IArticleDTO {
+    throw new Error('Method not implemented.');
+  }
+  putItem(id: TID): IArticleDTO {
+    throw new Error('Method not implemented.');
+  }
+  deleteItem(id: TID): IArticleDTO {
+    throw new Error('Method not implemented.');
+  }
+}
