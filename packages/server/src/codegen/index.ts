8a81ddc61662f4baf722698cb3de603775a08251
scub111 <scub111@mail.ru>
2019-09-04 23:50:58 +0300

    Temp commit.

--- packages/server/src/codegen/index.ts
+++ packages/server/src/codegen/index.ts
@@ -6,14 +6,55 @@ console.log('test');
 import { setEnvVariables } from 'common/serverConfig';
 setEnvVariables(__dirname + '/.env');
 
-import { createDTO } from './dto';
 import { getFields } from './getFields';
-import { createService } from './service';
+import { createSorce } from './generateSource';
+import appRootPath = require('app-root-path');
 
 const main = async () => {
-  const fields = await getFields('article');
-  createDTO('articles2', 'Article', fields);
-  createService('articles2', 'Articles', 'IArticleDTO', fields);
+  const tableName = 'Article';
+  const saveFolder = 'articles2';
+  const serviceName = 'Articles';
+
+  const interfaceName = `I${tableName}DTO`;
+  const filePrefixDTO = tableName.toLowerCase();
+  const filePrefix = serviceName.toLowerCase();
+
+  const templateRootPath = `${appRootPath.path}/src/codegen/templates`;
+  const sourceRootPath = `${appRootPath.path}/src/server`;
+
+  const fields = await getFields(tableName);
+
+  // Create *.dto.ts
+  createSorce({
+    templatePath: `${templateRootPath}/dto.hbs`,
+    data: {
+      interfaceName,
+      fields,
+    },
+    sourcePath: `${sourceRootPath}/${saveFolder}/${filePrefixDTO}.dto.ts`,
+  });
+
+  // Create *.service.ts
+  // createSorce({
+  //   templatePath: `${templateRootPath}/service.hbs`,
+  //   data: {
+  //     serviceName,
+  //     interfaceName,
+  //     fields,
+  //   },
+  //   sourcePath: `${sourceRootPath}/${saveFolder}/${filePrefix}.service.ts`,
+  // });
+
+  // Create *.controller.ts
+  // createSorce({
+  //   templatePath: `${templateRootPath}/controller.hbs`,
+  //   data: {
+  //     serviceName,
+  //     interfaceName,
+  //     fields,
+  //   },
+  //   sourcePath: `${sourceRootPath}/${saveFolder}/${filePrefix}.controller.ts`,
+  // });
 };
 
 main();
