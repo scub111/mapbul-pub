1876b86080d1a25574d8586b26875b3afe550521
scub111 <scub111@mail.ru>
2019-09-04 08:36:38 +0300

    Temp commit.

--- packages/server/packages/common/src/codegen/dto.ts
+++ packages/server/packages/common/src/codegen/dto.ts
@@ -1,17 +1,29 @@
 import { Project, StructureKind, OptionalKind, PropertySignatureStructure } from 'ts-morph';
 import { IField } from './IField';
+import appRootPath from 'app-root-path';
+import handlebars from 'handlebars';
+import fs from 'fs';
 
-export const createDTO = (project: Project, folder: string, name: string, fields: IField[]) => {
+export const createDTO = (folder: string, name: string, fields: IField[]) => {
   const fullName = `I${name}DTO`;
   const fileName = `src/server/${folder}/${name.toLowerCase()}.dto.ts`;
-  const properties: Array<OptionalKind<PropertySignatureStructure>> = fields.map(i => ({name: i.field, type: i.type}));
-  const myInterfaceFile = project.createSourceFile(fileName, {
-    statements: [{
-        kind: StructureKind.Interface,
-        name: fullName,
-        isExported: true,
-        properties,
-    }],
-  }, { overwrite: true });
-  return myInterfaceFile;
+  // const properties: Array<OptionalKind<PropertySignatureStructure>> = fields.map(i => ({name: i.field, type: i.type}));
+  // const myInterfaceFile = project.createSourceFile(fileName, {
+  //   statements: [{
+  //       kind: StructureKind.Interface,
+  //       name: fullName,
+  //       isExported: true,
+  //       properties,
+  //   }],
+  // }, { overwrite: true });
+  // return myInterfaceFile;
+
+  const data = {
+    title: 'practical node.js',
+    author: '@azat_co',
+    fields,
+  };
+  const source = fs.readFileSync(`${appRootPath.path}/src/codegen/dto.hbs`, 'utf-8');
+  const template = handlebars.compile(source);
+  const html = template(data);
 };
