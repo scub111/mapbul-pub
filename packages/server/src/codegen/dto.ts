8661ef6119e8b3f580067c16a6ab882ee7a7f85b
scub111 <scub111@mail.ru>
2019-09-04 09:09:52 +0300

    Work on teamplates and its engine.

--- packages/server/src/codegen/dto.ts
+++ packages/server/src/codegen/dto.ts
@@ -1,29 +1,16 @@
-import { Project, StructureKind, OptionalKind, PropertySignatureStructure } from 'ts-morph';
 import { IField } from './IField';
 import appRootPath from 'app-root-path';
 import handlebars from 'handlebars';
 import fs from 'fs';
 
-export const createDTO = (folder: string, name: string, fields: IField[]) => {
-  const fullName = `I${name}DTO`;
-  const fileName = `src/server/${folder}/${name.toLowerCase()}.dto.ts`;
-  // const properties: Array<OptionalKind<PropertySignatureStructure>> = fields.map(i => ({name: i.field, type: i.type}));
-  // const myInterfaceFile = project.createSourceFile(fileName, {
-  //   statements: [{
-  //       kind: StructureKind.Interface,
-  //       name: fullName,
-  //       isExported: true,
-  //       properties,
-  //   }],
-  // }, { overwrite: true });
-  // return myInterfaceFile;
-
+export const createDTO = (saveFolder: string, name: string, fields: IField[]) => {
   const data = {
-    title: 'practical node.js',
-    author: '@azat_co',
+    name: `I${name}DTO`,
     fields,
   };
-  const source = fs.readFileSync(`${appRootPath.path}/src/codegen/dto.hbs`, 'utf-8');
-  const template = handlebars.compile(source);
-  const html = template(data);
+  const templateFile = fs.readFileSync(`${appRootPath.path}/src/codegen/templates/dto.hbs`, 'utf-8');
+  const template = handlebars.compile(templateFile);
+  const source = template(data);
+  fs.writeFileSync(`src/server/${saveFolder}/${name.toLowerCase()}.dto.ts`, source);
+  console.log(source);
 };
