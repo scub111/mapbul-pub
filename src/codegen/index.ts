1f95b89ef3139690e60a01e70fcbe190dd0a1979
scub111 <scub111@mail.ru>
2019-09-04 08:36:38 +0300

    Temp commit.

--- src/codegen/index.ts
+++ src/codegen/index.ts
@@ -9,29 +9,15 @@ setEnvVariables(__dirname + '/.env');
 import { Project } from 'ts-morph';
 import { createDTO } from './dto';
 import { getFields } from './getFields';
-import appRootPath from 'app-root-path';
-import handlebars from 'handlebars';
-import fs from 'fs';
+
+
+interface ICodeData {
+  interfaceName: string;
+}
 
 const main = async () => {
-  // const project = new Project({
-  //   tsConfigFilePath: `${appRootPath.path}/tsconfig.json`,
-  // });
-  // project.addExistingSourceFiles('src/**/*.ts');
-  // const interfaceFile = createDTO(project, 'articles2', 'Article', fields);
-
   const fields = await getFields('article');
-
-  const data = {
-    title: 'practical node.js',
-    author: '@azat_co',
-    tags: ['express', 'node', 'javascript']
-  };
-  const source = fs.readFileSync(`${appRootPath.path}/src/codegen/dto.hbs`, 'utf-8');
-  const template = handlebars.compile(source);
-  const html = template(data);
-  console.log(html);
-  // project.save();
+  createDTO()
 };
 
 main();
