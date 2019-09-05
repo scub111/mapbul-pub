098c07cf43d8efc759efa003a9406d5a4272e1a4
scub111 <scub111@mail.ru>
2019-09-05 21:01:02 +0300

    Fully work attempt.

--- packages/server/packages/common/packages/server/src/server/app.module.ts
+++ packages/server/packages/common/packages/server/src/server/app.module.ts
@@ -7,10 +7,12 @@ import { BooksController } from 'server/books/books.controller';
 import { ArticlesController } from 'server/articles/articles.controller';
 import { Articles2Service } from 'server/articles2/articles2.service';
 import { Articles2Controller } from 'server/articles2/articles2.controller';
+import { Articles3Service } from './articles3/articles3.service';
+import { Articles3Controller } from './articles3/articles3.controller';
 
 @Module({
   imports: [],
-  providers: [AppService, BooksService, ArticlesService, Articles2Service],
-  controllers: [AppController, BooksController, ArticlesController, Articles2Controller]
+  providers: [AppService, BooksService, ArticlesService, Articles2Service, Articles3Service],
+  controllers: [AppController, BooksController, ArticlesController, Articles2Controller, Articles3Controller]
 })
 export class AppModule {}
