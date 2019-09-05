da07589eee272eaf144e2c8b61ee0081aa26f229
scub111 <scub111@mail.ru>
2019-09-06 01:00:56 +0300

    Huge code generation.

---
+++ packages/server/packages/common/packages/server/src/server/favorites_markers/favorites_marker.dto.ts
@@ -0,0 +1,5 @@
+export interface IFavorites_markerDTO {
+  id: number;
+  userId: number;
+  markerId: number;
+}
--- packages/server/packages/common/packages/server/src/server/weekdays/weekdays.controller.ts
+++ packages/server/packages/common/packages/server/src/server/weekdays/weekdays.controller.ts
@@ -1,31 +1,31 @@
 import { Controller, Get, Post, Put, Delete, Param } from '@nestjs/common';
 import { TID } from 'server/common/types';
 import { IController } from 'server/common/IController';
-import { {{interfaceName}} } from 'server/{{saveFolder}}/{{tableName}}.dto';
-import { {{serviceName}} } from './{{saveFolder}}.service';
+import { IWeekdayDTO } from 'server/weekdays/weekday.dto';
+import { WeekdaysService } from './weekdays.service';
 import { IGetParams } from 'server/common/interfaces';
 
-@Controller('{{controller}}')
-export class {{controllerName}} implements IController<{{interfaceName}}> {
-  constructor(private readonly service: {{serviceName}}) {}
+@Controller('weekdays')
+export class WeekdaysController implements IController<IWeekdayDTO> {
+  constructor(private readonly service: WeekdaysService) {}
 
   @Get()
-  async getAll(): Promise<{{interfaceName}}[]> {
+  async getAll(): Promise<IWeekdayDTO[]> {
     return this.service.getAll();
   }
 
   @Post()
-  postItem(item: {{interfaceName}}): {{interfaceName}} {
+  postItem(item: IWeekdayDTO): IWeekdayDTO {
     throw new Error('Method not implemented.');
   }
 
   @Put()
-  putAll(item: {{interfaceName}}): {{interfaceName}} {
+  putAll(item: IWeekdayDTO): IWeekdayDTO {
     throw new Error('Method not implemented.');
   }
 
   @Get(':id')
-  async getItem(@Param() params: IGetParams): Promise<{{interfaceName}}> {
+  async getItem(@Param() params: IGetParams): Promise<IWeekdayDTO> {
     return await this.service.getItem(params.id);
   }
 
@@ -35,11 +35,11 @@ export class {{controllerName}} implements IController<{{interfaceName}}> {
   }
 
   @Put(':id')
-  putItem(id: TID, item: {{interfaceName}}): {{interfaceName}} {
+  putItem(id: TID, item: IWeekdayDTO): IWeekdayDTO {
     throw new Error('Method not implemented.');
   }
 
-  deleteItem(id: TID): {{interfaceName}} {
+  deleteItem(id: TID): IWeekdayDTO {
     throw new Error('Method not implemented.');
   }
 }
