4d719e4bd1fcafa01516a83e1373406a692f9f26
scub111 <scub111@mail.ru>
2019-09-06 08:36:27 +0300

    Cleaning.

--- packages/server/src/server/marker_photos/marker_photos.dto.ts
+++
@@ -1,6 +0,0 @@
-export interface IMarker_photosDTO {
-  id: number;
-  markerId: number;
-  photo: string;
-  photoMini: string;
-}
--- packages/server/src/server/worktimes/worktime.dto.ts
+++
@@ -1,7 +0,0 @@
-export interface IWorktimeDTO {
-  id: number;
-  openTime: Date;
-  closeTime: Date;
-  markerId: number;
-  weekDayId: number;
-}
