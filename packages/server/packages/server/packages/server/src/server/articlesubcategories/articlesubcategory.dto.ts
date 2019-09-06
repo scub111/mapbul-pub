5f9a2156329a0f42622457e72e771318b12603a0
scub111 <scub111@mail.ru>
2019-09-06 08:36:27 +0300

    Cleaning.

--- packages/server/packages/server/packages/server/src/server/articlesubcategories/articlesubcategory.dto.ts
+++
@@ -1,5 +0,0 @@
-export interface IArticlesubcategoryDTO {
-  id: number;
-  articleId: number;
-  categoryId: number;
-}
--- packages/server/packages/server/packages/server/src/server/city_permissions/city_permission.dto.ts
+++
@@ -1,5 +0,0 @@
-export interface ICity_permissionDTO {
-  id: number;
-  cityId: number;
-  userId: number;
-}
