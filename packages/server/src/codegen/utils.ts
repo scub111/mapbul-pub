bc20e726adfce4e7e8a11be9fb2dfecc95e47ab7
scub111 <scub111@mail.ru>
2019-09-07 10:40:01 +0300

    Dist improvements.

--- packages/server/src/codegen/utils.ts
+++ packages/server/src/codegen/utils.ts
@@ -2,10 +2,19 @@ import path from 'path';
 import fs from 'fs';
 import mkdirp from 'mkdirp';
 
-export const writeFileSync = (filePath: string, data: string) => {
+export const makeSureSync = (filePath: string) => {
   const dir = path.dirname(filePath);
   if (!fs.existsSync(filePath)) {
     mkdirp.sync(dir);
   }
+};
+
+export const writeFileSync = (filePath: string, data: string) => {
+  makeSureSync(filePath);
   fs.writeFileSync(filePath, data, { encoding: 'utf8', flag: 'w' });
+};
+
+export const appendFileSync = (filePath: string, data: string) => {
+  makeSureSync(filePath);
+  fs.appendFileSync(filePath, data, { encoding: 'utf8', flag: 'w' });
 };
