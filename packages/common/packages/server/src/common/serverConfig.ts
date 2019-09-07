a05745115972072466194edd2fff68a1873362c0
scub111 <scub111@mail.ru>
2019-09-07 10:23:22 +0300

    Format improvements.

--- packages/common/packages/server/src/common/serverConfig.ts
+++ packages/common/packages/server/src/common/serverConfig.ts
@@ -6,8 +6,8 @@ export const setEnvVariables = (path: string) => {
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE
-    }
+      database: process.env.DB_DATABASE,
+    },
   };
 };
 
