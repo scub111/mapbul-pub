1eeed2a1b6c13d4d8a8d2a024e77bf694991b9b4
scub111 <scub111@mail.ru>
2019-09-07 10:14:16 +0300

    View for Api was added.

--- packages/server/src/codegen/generateSource.ts
+++ packages/server/src/codegen/generateSource.ts
@@ -1,7 +1,6 @@
 import handlebars from 'handlebars';
-import path from 'path';
 import fs from 'fs';
-import mkdirp from 'mkdirp';
+import { writeFileSync } from 'codegen/utils';
 
 interface ICreateSorceConfig {
   templatePath: string;
@@ -13,9 +12,5 @@ export const createSorce = (config: ICreateSorceConfig) => {
   const templateFile = fs.readFileSync(config.templatePath, 'utf-8');
   const template = handlebars.compile(templateFile);
   const source = template(config.data);
-  const dir = path.dirname(config.sourcePath);
-  if (!fs.existsSync(config.sourcePath)) {
-    mkdirp.sync(dir);
-  }
-  fs.writeFileSync(config.sourcePath, source, { encoding: 'utf8', flag: 'w' });
+  writeFileSync(config.sourcePath, source);
 };
