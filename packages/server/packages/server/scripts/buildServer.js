0fb4e816448f8ac0d0b3ae376c1bff2a28eca81d
scub111 <scub111@mail.ru>
2019-09-11 00:42:57 +0300

    Build scripts improvements.

--- packages/server/packages/server/scripts/buildServer.js
+++ packages/server/packages/server/scripts/buildServer.js
@@ -1,2 +1,10 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_extra_1 = __importDefault(require("fs-extra"));
+const app_root_path_1 = __importDefault(require("app-root-path"));
 console.log('test');
+fs_extra_1.default.copyFileSync(`${app_root_path_1.default.path}/src/server/views/api.hbs`, `${app_root_path_1.default.path}/dist2/`);
 //# sourceMappingURL=buildServer.js.map
