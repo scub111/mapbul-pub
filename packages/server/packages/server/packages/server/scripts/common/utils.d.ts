bf4581dc285f6a05c92aa27ed2f442ba2deb7374
scub111 <scub111@mail.ru>
2019-09-11 01:19:58 +0300

    Scripts modifications.

---
+++ packages/server/packages/server/packages/server/scripts/common/utils.d.ts
@@ -0,0 +1,6 @@
+export declare const makeSureSync: (filePath: string) => void;
+export declare const deleteFileSync: (filePath: string) => void;
+export declare const readFileSync: (filePath: string) => string;
+export declare const writeFileSync: (filePath: string, data: string) => void;
+export declare const appendFileSync: (filePath: string, data: string) => void;
+export declare const copyFileSync: (source: string, destination: string, flags?: number) => void;
