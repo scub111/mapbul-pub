f2553b274390b45d9d13926d883d98394628990a
scub111 <scub111@mail.ru>
2019-09-11 00:42:57 +0300

    Build scripts improvements.

--- packages/server/src/scripts/buildServer.ts
+++ packages/server/src/scripts/buildServer.ts
@@ -1,1 +1,7 @@
+import fs from 'fs-extra';
+import appRootPath from 'app-root-path';
+
 console.log('test');
+const srcDir = `${appRootPath.path}/src`;
+const distDir = `${appRootPath.path}/dist3`;
+fs.copyFileSync(`${srcDir}/server/views/api.hbs`, `${distDir}/api.hbs`);
