9a7a9777c3a1db3f0653c8bae05ac35e1ebf6c03
scub111 <scub111@mail.ru>
2019-09-11 01:19:58 +0300

    Scripts modifications.

--- packages/common/scripts/scripts/buildServer.js
+++ packages/common/scripts/scripts/buildServer.js
@@ -3,8 +3,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const fs_extra_1 = __importDefault(require("fs-extra"));
 const app_root_path_1 = __importDefault(require("app-root-path"));
+const utils_1 = require("../common/utils");
 console.log('test');
-fs_extra_1.default.copyFileSync(`${app_root_path_1.default.path}/src/server/views/api.hbs`, `${app_root_path_1.default.path}/dist2/`);
+const srcDir = `${app_root_path_1.default.path}/src`;
+const distDir = `${app_root_path_1.default.path}/dist4`;
+utils_1.copyFileSync(`${srcDir}/server/views/api.hbs`, `${distDir}/api.hbs`);
 //# sourceMappingURL=buildServer.js.map
