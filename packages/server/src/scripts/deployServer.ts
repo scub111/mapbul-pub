54d023db906a0e6d29a89ce1b012a8e264b04655
scub111 <scub111@mail.ru>
2019-09-12 01:33:27 +0300

    CI test.

--- packages/server/src/scripts/deployServer.ts
+++ packages/server/src/scripts/deployServer.ts
@@ -4,14 +4,14 @@ import { runSync } from '../common/processUtils';
 
 const main = () => {
   console.log('Stoping site...');
-  const output = runSync(`%WINDIR%\system32\inetsrv\appcmd stop site /site.name:"mapbul.pub"`);
+  const output = runSync(`%WINDIR%/system32/inetsrv/appcmd stop site /site.name:"mapbul.pub"`);
   console.log('Building...');
   output = runSync(`npm run server-build`);
   console.log(output);
   console.log('Copying Web.config...');
   copyFileSync(`${appRootPath.path}/iis/Web.config`, `${appRootPath.path}/Web.config`);
   console.log('Starting site...');
-  output = runSync(`%WINDIR%\system32\inetsrv\appcmd start site /site.name:"mapbul.pub"`);
+  output = runSync(`%WINDIR%/system32/inetsrv/appcmd start site /site.name:"mapbul.pub"`);
 };
 
 main();
