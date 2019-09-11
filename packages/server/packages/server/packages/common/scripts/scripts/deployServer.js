1ba1c7d4537a314d46bba9d073c077f3e32cbca3
scub111 <scub111@mail.ru>
2019-09-12 01:33:27 +0300

    CI test.

--- packages/server/packages/server/packages/common/scripts/scripts/deployServer.js
+++ packages/server/packages/server/packages/common/scripts/scripts/deployServer.js
@@ -8,14 +8,14 @@ const fileUtils_1 = require("../common/fileUtils");
 const processUtils_1 = require("../common/processUtils");
 const main = () => {
     console.log('Stoping site...');
-    const output = processUtils_1.runSync(`%WINDIR%\system32\inetsrv\appcmd stop site /site.name:"mapbul.pub"`);
+    const output = processUtils_1.runSync(`%WINDIR%/system32/inetsrv/appcmd stop site /site.name:"mapbul.pub"`);
     console.log('Building...');
     output = processUtils_1.runSync(`npm run server-build`);
     console.log(output);
     console.log('Copying Web.config...');
     fileUtils_1.copyFileSync(`${app_root_path_1.default.path}/iis/Web.config`, `${app_root_path_1.default.path}/Web.config`);
     console.log('Starting site...');
-    output = processUtils_1.runSync(`%WINDIR%\system32\inetsrv\appcmd start site /site.name:"mapbul.pub"`);
+    output = processUtils_1.runSync(`%WINDIR%/system32/inetsrv/appcmd start site /site.name:"mapbul.pub"`);
 };
 main();
 //# sourceMappingURL=deployServer.js.map
