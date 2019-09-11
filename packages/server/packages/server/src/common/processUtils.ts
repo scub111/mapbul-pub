bb72481463c9956c7c7a35b9a4adf37dd752f036
scub111 <scub111@mail.ru>
2019-09-11 10:37:17 +0300

    Build script stage.

--- packages/server/packages/server/src/common/processUtils.ts
+++ packages/server/packages/server/src/common/processUtils.ts
@@ -1,17 +1,8 @@
 import * as childProcess from 'child_process';
 
 export const runSync = (command: string) => {
-  childProcess.execSync(command);
+  return childProcess.execSync(command).toString();
 };
-
-// export const runAsync = async (command: string) => {
-//   return new Promise(async (resolve, reject) => {
-//       const process = childProcess.spawn(command);
-//       process.on('data', data => resolve(data));
-//       process.on('error', err => reject(err));
-//       process.on('close', err => reject(err));
-//   });
-// };
 
 export const runAsync = async (command: string) => {
   const exec = childProcess.exec;
