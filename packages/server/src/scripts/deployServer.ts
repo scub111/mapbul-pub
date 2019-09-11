58c847427059bfba675257ce06be3c198291f88d
scub111 <scub111@mail.ru>
2019-09-12 01:31:41 +0300

    Deploy script improvement.

--- packages/server/src/scripts/deployServer.ts
+++ packages/server/src/scripts/deployServer.ts
@@ -3,11 +3,15 @@ import { copyFileSync, removeDirSync, removeFileSync } from '../common/fileUtils
 import { runSync } from '../common/processUtils';
 
 const main = () => {
+  console.log('Stoping site...');
+  const output = runSync(`%WINDIR%\system32\inetsrv\appcmd stop site /site.name:"mapbul.pub"`);
   console.log('Building...');
-  const output = runSync(`npm run server-build`);
+  output = runSync(`npm run server-build`);
   console.log(output);
   console.log('Copying Web.config...');
   copyFileSync(`${appRootPath.path}/iis/Web.config`, `${appRootPath.path}/Web.config`);
+  console.log('Starting site...');
+  output = runSync(`%WINDIR%\system32\inetsrv\appcmd start site /site.name:"mapbul.pub"`);
 };
 
 main();
