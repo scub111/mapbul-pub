1d1cc51e2fc55a41fa85def9e27ceb2ad12614b0
scub111 <scub111@mail.ru>
2019-09-12 01:31:41 +0300

    Deploy script improvement.

--- packages/server/packages/common/packages/server/scripts/scripts/deployServer.js
+++ packages/server/packages/common/packages/server/scripts/scripts/deployServer.js
@@ -7,11 +7,15 @@ const app_root_path_1 = __importDefault(require("app-root-path"));
 const fileUtils_1 = require("../common/fileUtils");
 const processUtils_1 = require("../common/processUtils");
 const main = () => {
+    console.log('Stoping site...');
+    const output = processUtils_1.runSync(`%WINDIR%\system32\inetsrv\appcmd stop site /site.name:"mapbul.pub"`);
     console.log('Building...');
-    const output = processUtils_1.runSync(`npm run server-build`);
+    output = processUtils_1.runSync(`npm run server-build`);
     console.log(output);
     console.log('Copying Web.config...');
     fileUtils_1.copyFileSync(`${app_root_path_1.default.path}/iis/Web.config`, `${app_root_path_1.default.path}/Web.config`);
+    console.log('Starting site...');
+    output = processUtils_1.runSync(`%WINDIR%\system32\inetsrv\appcmd start site /site.name:"mapbul.pub"`);
 };
 main();
 //# sourceMappingURL=deployServer.js.map
