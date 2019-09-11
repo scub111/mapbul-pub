1c48d1241d6ba71e627ec18a64513f968db8cb10
scub111 <scub111@mail.ru>
2019-09-11 09:27:40 +0300

    First work attemp to build script.

--- src/scripts/buildServer.ts
+++ src/scripts/buildServer.ts
@@ -1,8 +1,19 @@
 import appRootPath from 'app-root-path';
-import { copyFileSync, removeDirSync } from '../common/utils';
+import { copyFileSync, removeDirSync } from '../common/fileUtils';
+import { runSync, runAsync } from '../common/processUtils';
 
-console.log('test');
-const srcDir = `${appRootPath.path}/src`;
-const distDir = `${appRootPath.path}/dist`;
-removeDirSync(distDir);
-copyFileSync(`${srcDir}/server/views/api.hbs`, `${distDir}/api.hbs`);
+const bootstrap = async () => {
+  console.log('Build was started');
+  const srcDir = `${appRootPath.path}/src`;
+  const distDir = `${appRootPath.path}/dist`;
+  console.log('Dist folder is removing...');
+  removeDirSync(distDir);
+  console.log('Files are copying...');
+  copyFileSync(`${srcDir}/server/views/api.hbs`, `${distDir}/api.hbs`);
+  // const output = await runAsync('npm -v');
+  console.log('Compiling...');
+  const output = await runAsync(`tsc -p ${appRootPath.path}/tsconfig.server-build.json --diagnostics`);
+  console.log(output);
+};
+
+bootstrap();
