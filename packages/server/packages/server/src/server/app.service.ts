cd329f5eae750e6072b5e5ac3f1e04d1c32060b7
scub111 <scub111@mail.ru>
2019-09-12 01:40:34 +0300

    CI test.

--- packages/server/packages/server/src/server/app.service.ts
+++ packages/server/packages/server/src/server/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello, MapBul-scub111!';
+    return 'Hello, MapBul-scub!';
   }
 }
