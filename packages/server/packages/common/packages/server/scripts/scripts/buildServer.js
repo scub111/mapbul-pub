dd091ca5aa2b5cb9f0aa1ee21302386c2bc278b0
scub111 <scub111@mail.ru>
2019-09-11 10:37:17 +0300

    Build script stage.

--- packages/server/packages/common/packages/server/scripts/scripts/buildServer.js
+++ packages/server/packages/common/packages/server/scripts/scripts/buildServer.js
@@ -1,12 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -14,17 +6,21 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const app_root_path_1 = __importDefault(require("app-root-path"));
 const fileUtils_1 = require("../common/fileUtils");
 const processUtils_1 = require("../common/processUtils");
-const bootstrap = () => __awaiter(this, void 0, void 0, function* () {
-    console.log('Build was started');
-    const srcDir = `${app_root_path_1.default.path}/src`;
-    const distDir = `${app_root_path_1.default.path}/dist`;
+const main = () => {
+    console.log('Build was started 2');
+    const srcDir = `${app_root_path_1.default.path}/src/server`;
+    const distDir = `${app_root_path_1.default.path}/dist/server`;
     console.log('Dist folder is removing...');
     fileUtils_1.removeDirSync(distDir);
     console.log('Files are copying...');
-    fileUtils_1.copyFileSync(`${srcDir}/server/views/api.hbs`, `${distDir}/api.hbs`);
+    fileUtils_1.copyFileSync(`${srcDir}/.env`, `${distDir}/.env`);
+    fileUtils_1.copyFileSync(`${srcDir}/views/api.hbs`, `${distDir}/views/api.hbs`);
+    fileUtils_1.copyFileSync(`${srcDir}/api.txt`, `${distDir}/api.txt`);
     console.log('Compiling...');
-    const output = yield processUtils_1.runAsync(`tsc -p ${app_root_path_1.default.path}/tsconfig.server-build.json --diagnostics`);
+    const output = processUtils_1.runSync(`tsc -p ${app_root_path_1.default.path}/tsconfig.server-build.json --diagnostics`);
     console.log(output);
-});
-bootstrap();
+    console.log('Cleaning...');
+    fileUtils_1.removeFileSync(`${distDir}/tsconfig.server-build.tsbuildinfo`);
+};
+main();
 //# sourceMappingURL=buildServer.js.map
