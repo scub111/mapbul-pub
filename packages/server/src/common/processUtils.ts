58c156170cdbbb6bd110babc5355acf11d240ebb
scub111 <scub111@mail.ru>
2019-09-16 22:46:48 +0300

    Format.

--- packages/server/src/common/processUtils.ts
+++ packages/server/src/common/processUtils.ts
@@ -7,11 +7,11 @@ export const runSync = (command: string) => {
 export const runAsync = async (command: string) => {
   const exec = childProcess.exec;
   return new Promise((resolve, reject) => {
-   exec(command, (error, stdout, stderr) => {
-    if (error) {
-     console.warn(error);
-    }
-    resolve(stdout ? stdout : stderr);
-   });
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.warn(error);
+      }
+      resolve(stdout ? stdout : stderr);
+    });
   });
- }
+};
