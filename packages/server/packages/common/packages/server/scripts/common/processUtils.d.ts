ae78a0390c5b0391afc278fb855cc27de547ba06
scub111 <scub111@mail.ru>
2019-09-19 09:04:20 +0300

    Temp commit.

--- packages/server/packages/common/packages/server/scripts/common/processUtils.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const runSync: (command: string) => string;
-export declare const runAsync: (command: string) => Promise<{}>;
--- packages/server/packages/common/src/article.dto.ts
+++
@@ -1,3 +0,0 @@
-export interface IArticleDTO {
-  id: string;
-}
