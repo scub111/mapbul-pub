5a74e7fa08ab1b721dc598cc5528e59ce51449a8
scub111 <scub111@mail.ru>
2019-11-04 16:28:38 +0300

    Temp commit.

--- packages/scripts/src/deployServer.ts
+++ packages/scripts/src/deployServer.ts
@@ -5,7 +5,7 @@ export const deployServer = () => {
   console.log('Stoping site mapbul.pub...');
   let output = runSync(`%WINDIR%/system32/inetsrv/appcmd stop site /site.name:"mapbul.pub"`);
   console.log('Building...');
-  output = runSync(`npm run server-build`);
+  output = runSync(`npm run build:deploy`);
   console.log(output);
   // console.log('Copying Web.config...');
   // copyFileSync(`${appRootPath.path}/iis/Web.config`, `${appRootPath.path}/Web.config`);
--- scripts/deployServer.js
+++ scripts/deployServer.js
@@ -5,7 +5,7 @@ exports.deployServer = () => {
     console.log('Stoping site mapbul.pub...');
     let output = common_1.runSync(`%WINDIR%/system32/inetsrv/appcmd stop site /site.name:"mapbul.pub"`);
     console.log('Building...');
-    output = common_1.runSync(`npm run server-build`);
+    output = common_1.runSync(`npm run build:deploy`);
     console.log(output);
     console.log('Starting site mapbul.pub...');
     output = common_1.runSync(`%WINDIR%/system32/inetsrv/appcmd start site /site.name:"mapbul.pub"`);
