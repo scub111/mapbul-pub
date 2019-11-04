bd2d078ee73a0e32f155309bcf60601d70e38570
scub111 <scub111@mail.ru>
2019-11-04 18:04:16 +0300

    CI improvement.

--- packages/server/scripts/buildServer.js
+++ packages/server/scripts/buildServer.js
@@ -4,19 +4,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_root_path_1 = __importDefault(require("app-root-path"));
-const common_1 = require("@mapbul-pub/common");
+const scub111_common_1 = require("scub111-common");
 exports.buildServer = () => {
     console.log('Build was started');
     console.log('Compiling...');
-    const output = common_1.runSync(`npm run build:init`);
+    const output = scub111_common_1.runSync(`npm run build:init`);
     console.log(output);
     const srcDir = `${app_root_path_1.default.path}/src`;
     const distDir = `${app_root_path_1.default.path}/dist`;
     console.log('Files are copying...');
-    common_1.copyFileSync(`${srcDir}/.env`, `${distDir}/.env`);
-    common_1.copyFileSync(`${srcDir}/views/api.hbs`, `${distDir}/views/api.hbs`);
-    common_1.copyFileSync(`${srcDir}/api.txt`, `${distDir}/api.txt`);
+    scub111_common_1.copyFileSync(`${srcDir}/.env`, `${distDir}/.env`);
+    scub111_common_1.copyFileSync(`${srcDir}/views/api.hbs`, `${distDir}/views/api.hbs`);
+    scub111_common_1.copyFileSync(`${srcDir}/api.txt`, `${distDir}/api.txt`);
     console.log('Cleaning...');
-    common_1.removeFileSync(`${distDir}/tsconfig.server-build.tsbuildinfo`);
+    scub111_common_1.removeFileSync(`${distDir}/tsconfig.server-build.tsbuildinfo`);
 };
 exports.buildServer();
--- scripts/deployServer.js
+++ scripts/deployServer.js
@@ -1,13 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const common_1 = require("@mapbul-pub/common");
+const scub111_common_1 = require("scub111-common");
 exports.deployServer = () => {
     console.log('Stoping site mapbul.pub...');
-    let output = common_1.runSync(`%WINDIR%/system32/inetsrv/appcmd stop site /site.name:"mapbul.pub"`);
+    let output = scub111_common_1.runSync(`%WINDIR%/system32/inetsrv/appcmd stop site /site.name:"mapbul.pub"`);
     console.log('Building...');
-    output = common_1.runSync(`npm run build:deploy`);
+    output = scub111_common_1.runSync(`npm run build:deploy`);
     console.log(output);
     console.log('Starting site mapbul.pub...');
-    output = common_1.runSync(`%WINDIR%/system32/inetsrv/appcmd start site /site.name:"mapbul.pub"`);
+    output = scub111_common_1.runSync(`%WINDIR%/system32/inetsrv/appcmd start site /site.name:"mapbul.pub"`);
 };
 exports.deployServer();
