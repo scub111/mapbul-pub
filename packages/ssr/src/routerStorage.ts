89b377e08563ae618263e3cada6e0d442b004d51
Konstatin Biskub <kbiskub@web3tech.ru>
2019-11-05 08:38:03 +0300

    SSR first commit.

---
+++ packages/ssr/src/routerStorage.ts
@@ -0,0 +1,18 @@
+import * as path from 'path';
+import appRootPath from 'app-root-path';
+import { removeFileSync, readFileSync, appendFileSync } from 'scub111-common';
+
+const apiPath = path.join(appRootPath.path, '..', 'server/src/api.txt');
+console.log(apiPath);
+
+export const deleteRouterSync = () => {
+  removeFileSync(apiPath);
+};
+
+export const readRouterSync = (path: string = apiPath) => {
+  return readFileSync(path);
+};
+
+export const appendRouterSync = (data: string) => {
+  appendFileSync(apiPath, `${data} \n`);
+};
