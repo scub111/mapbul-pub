d801f0854b573a11819a6d5edd7821e09d0716f8
scub111 <scub111@mail.ru>
2019-11-16 15:01:40 +0300

    CI test.

--- scripts/deployServer.js
+++ scripts/deployServer.js
@@ -2,12 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const scub111_common_1 = require("scub111-common");
 exports.deployServer = () => {
-    console.log('Stoping site mapbul.pub...');
-    let output = scub111_common_1.runSync(`%WINDIR%/system32/inetsrv/appcmd stop site /site.name:"mapbul.pub"`);
+    console.log('Stoping site mapbul.api...');
+    let output = scub111_common_1.runSync(`%WINDIR%/system32/inetsrv/appcmd stop site /site.name:"mapbul.api"`);
     console.log('Building...');
     output = scub111_common_1.runSync(`npm run deploy:init`);
     console.log(output);
-    console.log('Starting site mapbul.pub...');
-    output = scub111_common_1.runSync(`%WINDIR%/system32/inetsrv/appcmd start site /site.name:"mapbul.pub"`);
+    console.log('Starting site mapbul.api...');
+    output = scub111_common_1.runSync(`%WINDIR%/system32/inetsrv/appcmd start site /site.name:"mapbul.api"`);
 };
 exports.deployServer();
--- src/deployServer.ts
+++ src/deployServer.ts
@@ -2,15 +2,15 @@
 import { runSync, copyFileSync } from 'scub111-common';
 
 export const deployServer = () => {
-  console.log('Stoping site mapbul.pub...');
-  let output = runSync(`%WINDIR%/system32/inetsrv/appcmd stop site /site.name:"mapbul.pub"`);
+  console.log('Stoping site mapbul.api...');
+  let output = runSync(`%WINDIR%/system32/inetsrv/appcmd stop site /site.name:"mapbul.api"`);
   console.log('Building...');
   output = runSync(`npm run deploy:init`);
   console.log(output);
   // console.log('Copying Web.config...');
   // copyFileSync(`${appRootPath.path}/iis/Web.config`, `${appRootPath.path}/Web.config`);
-  console.log('Starting site mapbul.pub...');
-  output = runSync(`%WINDIR%/system32/inetsrv/appcmd start site /site.name:"mapbul.pub"`);
+  console.log('Starting site mapbul.api...');
+  output = runSync(`%WINDIR%/system32/inetsrv/appcmd start site /site.name:"mapbul.api"`);
 };
 
 deployServer();
