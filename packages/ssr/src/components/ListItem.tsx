fd066a8bf5d95d4c24d33f4a07e3a290d79aed35
Konstatin Biskub <kbiskub@web3tech.ru>
2019-11-22 09:33:56 +0300

    Ssr. Temp commit.

--- packages/ssr/src/components/ListItem.tsx
+++ packages/ssr/src/components/ListItem.tsx
@@ -1,58 +1,16 @@
 import * as React from 'react'
-import { IArticleDTO } from "@mapbul-pub/types"
-import { IMAGE_URL } from '../common/constants';
-import Link from '../Link';
+// import { IArticleDTO } from "@mapbul-pub/types"
+// import { IMAGE_URL } from '../common/constants';
+// import Link from '../Link';
 import { Grid, CardActionArea, Card, CardContent, Typography, Hidden, CardMedia, makeStyles } from '@material-ui/core';
-import { clearUrl } from '../utils/urlUtils';
+// import { clearUrl } from '../utils/urlUtils';
 
 type Props = {
-  item: IArticleDTO
+  // item: IArticleDTO
+  item: any
 }
 
-const useStyles = makeStyles(theme => ({
-  toolbar: {
-    borderBottom: `1px solid ${theme.palette.divider}`,
-  },
-  toolbarTitle: {
-    flex: 1,
-  },
-  toolbarSecondary: {
-    justifyContent: 'space-between',
-    overflowX: 'auto',
-  },
-  toolbarLink: {
-    padding: theme.spacing(1),
-    flexShrink: 0,
-  },
-  mainFeaturedPost: {
-    position: 'relative',
-    backgroundColor: theme.palette.grey[800],
-    color: theme.palette.common.white,
-    marginBottom: theme.spacing(4),
-    backgroundImage: 'url(https://source.unsplash.com/user/erondu)',
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
-    backgroundPosition: 'center',
-  },
-  overlay: {
-    position: 'absolute',
-    top: 0,
-    bottom: 0,
-    right: 0,
-    left: 0,
-    backgroundColor: 'rgba(0,0,0,.3)',
-  },
-  mainFeaturedPostContent: {
-    position: 'relative',
-    padding: theme.spacing(3),
-    [theme.breakpoints.up('md')]: {
-      padding: theme.spacing(6),
-      paddingRight: 0,
-    },
-  },
-  mainGrid: {
-    marginTop: theme.spacing(3),
-  },
+const useStyles = makeStyles(() => ({
   card: {
     display: 'flex',
   },
@@ -62,64 +20,41 @@ const useStyles = makeStyles(theme => ({
   cardMedia: {
     width: 160,
   },
-  markdown: {
-    ...theme.typography.body2,
-    padding: theme.spacing(3, 0),
-  },
-  sidebarAboutBox: {
-    padding: theme.spacing(2),
-    backgroundColor: theme.palette.grey[200],
-  },
-  sidebarSection: {
-    marginTop: theme.spacing(3),
-  },
-  footer: {
-    backgroundColor: theme.palette.background.paper,
-    marginTop: theme.spacing(8),
-    padding: theme.spacing(6, 0),
-  },
 }));
 
 const ListItem: React.FunctionComponent<Props> = ({ item }) => {
   const classes = useStyles();
 
   return (
-    <div style={{ display: 'flex', alignItems: 'center' }}>
-      <Link href="/articles/[id]" as={`/articles/${item.id}`}>
-        <Grid item key={item.title} xs={12} md={6}>
-          <CardActionArea component="a" href="#">
-            <Card className={classes.card}>
-              <div className={classes.cardDetails}>
-                <CardContent>
-                  <Typography component="h2" variant="h5">
-                    {item.title}
-                  </Typography>
-                  <Typography variant="subtitle1" color="textSecondary">
-                    {item.addedDate}
-                  </Typography>
-                  <Typography variant="subtitle1" paragraph>
-                    {item.description}
-                  </Typography>
-                  <Typography variant="subtitle1" color="primary">
-                    Continue reading...
-              </Typography>
-                </CardContent>
-              </div>
-              {item.titlePhoto && (
-                <Hidden xsDown>
-                  <CardMedia
-                    className={classes.cardMedia}
-                    // image="https://source.unsplash.com/random2"
-                    image={clearUrl(`${IMAGE_URL}/${item.titlePhoto}`)}
-                    title="Image title"
-                  />
-                </Hidden>
-              )}
-            </Card>
-          </CardActionArea>
-        </Grid>
-      </Link>
-    </div>
+    <Grid item key={item.title} xs={12} md={6}>
+    <CardActionArea component="a" href="#">
+      <Card className={classes.card}>
+        <div className={classes.cardDetails}>
+          <CardContent>
+            <Typography component="h2" variant="h5">
+              {item.title}
+            </Typography>
+            <Typography variant="subtitle1" color="textSecondary">
+              {item.date}
+            </Typography>
+            <Typography variant="subtitle1" paragraph>
+              {item.description}
+            </Typography>
+            <Typography variant="subtitle1" color="primary">
+              Continue reading...
+            </Typography>
+          </CardContent>
+        </div>
+        <Hidden xsDown>
+          <CardMedia
+            className={classes.cardMedia}
+            image="https://source.unsplash.com/random"
+            title="Image title"
+          />
+        </Hidden>
+      </Card>
+    </CardActionArea>
+    </Grid>
   )
 }
 
