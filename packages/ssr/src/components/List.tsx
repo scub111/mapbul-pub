05b1cee4c1dbb8815eba2c3c40aedf2aadbc5675
Konstatin Biskub <kbiskub@web3tech.ru>
2019-11-22 21:27:04 +0300

    Ssr. Styling fix.

--- packages/ssr/src/components/List.tsx
+++ packages/ssr/src/components/List.tsx
@@ -11,35 +11,35 @@ type Props = {
   items: Array<IArticleDTO>
 }
 
-const featuredPosts = [
-  {
-    title: 'Featured post 1',
-    date: 'Nov 12',
-    description:
-      'This is a wider card with supporting text below as a natural lead-in to additional content.',
-  },
-  {
-    title: 'Post title 2',
-    date: 'Nov 11',
-    description:
-      'This is a wider card with supporting text below as a natural lead-in to additional content.',
-  },
-];
+// const featuredPosts = [
+//   {
+//     title: 'Featured post 1',
+//     date: 'Nov 12',
+//     description:
+//       'This is a wider card with supporting text below as a natural lead-in to additional content.',
+//   },
+//   {
+//     title: 'Post title 2',
+//     date: 'Nov 11',
+//     description:
+//       'This is a wider card with supporting text below as a natural lead-in to additional content.',
+//   },
+// ];
 
-const List: React.FunctionComponent<Props> = () => {
+const List: React.FunctionComponent<Props> = ({ items }) => {
   return (
     <>
       {/* <Blog/> */}
       <CssBaseline />
       <Container maxWidth="lg">
         <Grid container spacing={4}>
-          {/* {items.map((item: IArticleDTO) => (
-            <ListItem item={item} />
-          ))} */}
-
-          {featuredPosts.map(post => (
-            <ListItem item={post} />
+          {items.map((item: IArticleDTO, index: number) => (
+            <ListItem key={index} item={item} />
           ))}
+
+          {/* {featuredPosts.map(post => (
+            <ListItem item={post} />
+          ))} */}
         </Grid>
       </Container>
     </>
--- packages/ssr/src/components/ListDetail.tsx
+++ packages/ssr/src/components/ListDetail.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { IArticleDTO } from "@mapbul-pub/types"
 import { IMAGE_URL } from '../common/constants';
+import { clearUrl } from '../utils/urlUtils';
 
 type ListDetailProps = {
   item: IArticleDTO
@@ -9,7 +10,7 @@ type ListDetailProps = {
 const ListDetail: React.FunctionComponent<ListDetailProps> = ({ item }) => (
   <div>
     <h1>{item.title}</h1>
-    <img src={`${IMAGE_URL}/${item.photo}`}></img>
+    <img src={clearUrl(`${IMAGE_URL}/${item.photo}`)}></img>
     <p>ID: {item.id}</p>
   </div>
 )
