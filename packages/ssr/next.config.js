dacf38a8100d309a831a192497877af7e7c11f78
scub111 <scub111@mail.ru>
2019-12-16 00:57:27 +0300

    SSR. Optimized next.config.js.

--- packages/ssr/next.config.js
+++ packages/ssr/next.config.js
@@ -1,48 +1,20 @@
-var path = require('path');
-const withPlugins = require('next-compose-plugins');
-
-// module.exports = withPlugins([{
-//   // Will only be available on the server side
-//   serverRuntimeConfig: {
-//   },
-//   // Will be available on both server and client
-//   publicRuntimeConfig: {
-//     BASE_URL: process.env.BASE_URL,
-//   },
-// }], {
-//   webpack(config, options) {
-//     config.resolve.alias['ssr'] = path.join(__dirname);
-//     console.log(config.resolve.alias);
-//     return config
-//   },
-// });
-
-
-// module.exports = (phase, { defaultConfig }) => {
-//   return {
-//     publicRuntimeConfig: {
-//       BASE_URL: process.env.BASE_URL,
-//     },
-//     webpack: (config, { buildId, dev, isServer, defaultLoaders }) => {
-//       config.resolve.alias['ssr'] = path.join(__dirname);
-//       console.log(config.resolve.alias);
-//       return config
-//     },
-//   };
-// };
-
 require('dotenv').config();
-module.exports = {
-  // Will only be available on the server side
-  serverRuntimeConfig: {
-  },
-  // Will be available on both server and client
-  publicRuntimeConfig: {
-    BASE_URL: process.env.BASE_URL,
-  },
-  webpack: (config, { buildId, dev, isServer, defaultLoaders }) => {
-    config.resolve.alias['ssr'] = path.join(__dirname);
-    console.log(config.resolve.alias);
-    return config
-  }
+var path = require('path');
+
+module.exports = (phase, { defaultConfig }) => {
+  return {
+    // Will only be available on the server side
+    serverRuntimeConfig: {
+    },
+    // Will be available on both server and client
+    publicRuntimeConfig: {
+      BASE_URL: process.env.BASE_URL,
+    },
+    webpack: (config, { buildId, dev, isServer, defaultLoaders }) => {
+      let alias = config.resolve.alias;
+      alias['ssr'] = path.join(__dirname);
+      console.log(config.resolve.alias);
+      return config
+    },
+  };
 };
