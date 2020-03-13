609f2a830f871231f615759ade9caedfffe07280
Konstatin Biskub <kbiskub@web3tech.ru>
2020-03-13 11:23:52 +0300

    Styling

--- packages/ssr/src/components/List.tsx
+++ packages/ssr/src/components/List.tsx
@@ -2,10 +2,11 @@ import * as React from 'react';
 import { ListItem } from './ListItem';
 import { Grid } from '@material-ui/core';
 import { Article } from '../models/Article';
+import { CommonProps } from '@material-ui/core/OverridableComponent';
 
-export const List: React.FC<{ items: Array<Article>; route: string }> = ({ items, route }) => {
+export const List: React.FC<{ items: Array<Article>; route: string  } & CommonProps<any>> = ({ items, route, ...props }) => {
   return (
-    <Grid container spacing={2}>
+    <Grid container spacing={2} {...props}>
       {items.map((item: Article, index: number) => (
         <ListItem key={index} item={item} route={route} />
       ))}
