b64eb29d00c70e890287a376dd42d8c4ef21d898
scub111 <scub111@mail.ru>
2020-05-01 00:33:01 +0300

    Erase some warnings

--- packages/ssr/src/components/Footer.tsx
+++ packages/ssr/src/components/Footer.tsx
@@ -3,25 +3,23 @@ import { Container, Typography } from '@material-ui/core';
 import { useTranslation } from 'hooks';
 import { IStyleProps } from 'interfaces';
 
-const Copyright:React.FC = () => {
+const Copyright: React.FC = () => {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
       {new Date().getFullYear()}
     </Typography>
   );
-}
+};
 
 export const Footer: React.FC<IStyleProps> = ({ style }) => {
   const { t } = useTranslation();
   return (
-    <p>
-      <Container maxWidth="lg" style={{ ...style }}>
-        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
-          {t('allRightsReserved')}
-        </Typography>
-        <Copyright />
-      </Container>
-    </p>
+    <Container maxWidth="lg" style={{ ...style }}>
+      <Typography variant="subtitle1" align="center" color="textSecondary">
+        {t('allRightsReserved')}
+      </Typography>
+      <Copyright />
+    </Container>
   );
 };
