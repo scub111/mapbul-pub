cf3bbb33c820dc1bbc515893ebcfa87275c08164
Konstatin Biskub <kbiskub@web3tech.ru>
2020-07-01 12:20:17 +0300

    ci

--- packages/server/src/api/db/db.controller.ts
+++ packages/server/src/api/db/db.controller.ts
@@ -1,14 +1,14 @@
-import { Controller, Get, HttpException, HttpStatus } from '@nestjs/common';
-// import { GlobalVar } from '@mapbul-pub/common';
+import { Controller, Get, Render} from '@nestjs/common';
+import { GlobalVar } from '@mapbul-pub/common';
 
 @Controller('api/db')
 export class DbController {
   @Get()
-  // @Render('db')
+  @Render('db')
   root() {
     // throw "some trouble123";
-    throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
-    // return { dbConnection: GlobalVar.env.dbConnection.host };
+    // throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
+    return { dbConnection: GlobalVar.env.dbConnection.host };
     return { dbConnection: "test" };
   }
 }
