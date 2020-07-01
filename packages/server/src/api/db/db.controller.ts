ce9d51632afa3fa73385ab65456a097d4bf74a79
Konstatin Biskub <kbiskub@web3tech.ru>
2020-07-01 12:04:48 +0300

    ci

--- packages/server/src/api/db/db.controller.ts
+++ packages/server/src/api/db/db.controller.ts
@@ -6,7 +6,7 @@ export class DbController {
   @Get()
   // @Render('db')
   root() {
-    throw "some trouble123";
+    // throw "some trouble123";
     throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
     // return { dbConnection: GlobalVar.env.dbConnection.host };
     return { dbConnection: "test" };
