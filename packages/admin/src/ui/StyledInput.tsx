9b75125849ca215b25d39d7de441c30afaabd8df
Admin <Admin@WIN-FD8M3O06T3N>
2020-07-05 00:06:16 +0300

    Temp commit

--- packages/admin/src/ui/StyledInput.tsx
+++
@@ -1,9 +0,0 @@
-// import * as React from 'react';
-import styled from 'styled-components';
-import { Input } from '@we-ui-components/form';
-
-export const StyledInput = styled(Input)`
-  ::placeholder {
-    color: ${props => props.theme.palette.Basic500};
-  }
-`;
--- packages/admin/src/utils/app-basename.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-
-type BaseName = string;
-
-const AppBaseNameContext = React.createContext<BaseName>(undefined);
-
-export const AppBaseNameProvider = AppBaseNameContext.Provider;
-export const useAppBaseName = () => React.useContext(AppBaseNameContext);
