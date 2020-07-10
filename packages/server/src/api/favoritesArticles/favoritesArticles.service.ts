6ac1e808441b6f7bd4c7dabda9dd077851ca7929
Admin <Admin@WIN-FD8M3O06T3N>
2020-07-10 20:24:35 +0300

    Codegen

--- packages/server/src/api/favoritesArticles/favoritesArticles.service.ts
+++ packages/server/src/api/favoritesArticles/favoritesArticles.service.ts
@@ -15,7 +15,7 @@ export class FavoritesArticlesService implements BaseService<IFavoritesArticleDT
     if (query.filter) {
       filter += `WHERE ${query.filter}`;
     }
-    let sort = ''; 
+    let sort = '';
     if (query.sort) {
       sort += `ORDER BY ${query.sort}`;
     }
--- packages/server/src/api/guides/guides.service.ts
+++ packages/server/src/api/guides/guides.service.ts
@@ -15,7 +15,7 @@ export class GuidesService implements BaseService<IGuideDTO> {
     if (query.filter) {
       filter += `WHERE ${query.filter}`;
     }
-    let sort = ''; 
+    let sort = '';
     if (query.sort) {
       sort += `ORDER BY ${query.sort}`;
     }
