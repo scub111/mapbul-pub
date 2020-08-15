bc677c5a22fc456c840dd61de1423d3b5245ac3d
Admin <Admin@WIN-FD8M3O06T3N>
2020-08-15 12:51:44 +0300

    Temp

--- packages/admin/src/pages/Categories/Poster.tsx
+++ packages/admin/src/pages/Categories/Poster.tsx
@@ -2,8 +2,11 @@ import * as React from 'react';
 import { CSSProperties } from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import { useImageInfo } from 'hooks';
+import { Typography } from '@material-ui/core';
 
-const Poster: React.FC<{src: string, style?: CSSProperties}> = ({ src, style }) => {
+const Poster: React.FC<{ src: string, style?: CSSProperties }> = ({ src, style }) => {
+  const [width, height] = useImageInfo(src);
   return (
     <Card style={{ display: 'inline-block', marginTop: '1em', zIndex: 2, ...style }}>
       <CardContent style={{ padding: 0 }}>
@@ -14,6 +17,9 @@ const Poster: React.FC<{src: string, style?: CSSProperties}> = ({ src, style }) 
             maxWidth: '100%',
             maxHeight: '30em',
           }} />
+        <Typography variant="subtitle1" paragraph component="h6" style={{ marginBottom: 0 }}>
+          {`${width}x${height}`}
+        </Typography>
       </CardContent>
     </Card>
   );
