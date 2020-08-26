e9ec690d8d53f0a48864c41d751bd2c45310d8fc
Konstatin Biskub <kbiskub@web3tech.ru>
2020-08-26 21:20:17 +0300

    ci

--- packages/server/src/api/auth/jwt.strategy.ts
+++ packages/server/src/api/auth/jwt.strategy.ts
@@ -2,7 +2,7 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { jwtConstants } from './constants';
-import { IUserDTO } from 'packages/types';
+import { IUserDTO } from '@mapbul-pub/types';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
